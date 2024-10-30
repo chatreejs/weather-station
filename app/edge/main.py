6027ed1640d99799cb95930476854ca46315e637
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-10-30 23:13:37 +0700

    feat: update message detail

--- app/edge/main.py
+++ app/edge/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import platform
 import signal
 import sys
 import threading
@@ -11,6 +12,9 @@ from dotenv import load_dotenv
 from kafka import KafkaProducer
 from models import SensorUpdate
 from sensors import BME280, PM1006
+
+APP_VERSION = "0.1.0"
+MOCK_PROBE_ID = "TH-10-0001"
 
 exit_flag = False
 
@@ -27,11 +31,11 @@ def get_boolean_from_string(value: str):
     if value_capitalize in ["True", "False"]:
         return value_capitalize == "True"
     else:
-        raise ValueError(f"invalid value: {value}")
+        raise ValueError(f"Invalid value: {value}")
 
 
 def send_message(message: dict, header_type: str):
-    logger.info(f"sending message: {message}")
+    logger.info(f"Sending message: {message}")
     producer.send(
         topic=KAFKA_PRODUCER_TOPIC,
         key=bytes("data", "utf-8"),
@@ -42,6 +46,7 @@ def send_message(message: dict, header_type: str):
 
 
 def weather_sensor_loop():
+    logger.info("Starting weather sensor loop")
     bme280 = BME280()
 
     previous_temperature = None
@@ -61,9 +66,14 @@ def weather_sensor_loop():
         ):
             sensor_data = SensorUpdate(
                 source=KAFKA_PRODUCER_SOURCE_NAME + "." + "bme280",
+                probe_id=MOCK_PROBE_ID,
                 type="temperature",
                 value=temperature,
                 time_of_event=current_datetime.isoformat(),
+                device=DEVICE_NAME,
+                manufacturer=DEVICE_MANUFACTURER,
+                platform=platform.platform(),
+                app_version=APP_VERSION,
             )
             message = sensor_data.to_dict()
             send_message(message, "SensorUpdate")
@@ -72,9 +82,14 @@ def weather_sensor_loop():
         if humidity is not None and previous_humidity != humidity and ENABLE_HUMIDITY:
             sensor_data = SensorUpdate(
                 source=KAFKA_PRODUCER_SOURCE_NAME + "." + "bme280",
+                probe_id=MOCK_PROBE_ID,
                 type="humidity",
                 value=humidity,
                 time_of_event=current_datetime.isoformat(),
+                device=DEVICE_NAME,
+                manufacturer=DEVICE_MANUFACTURER,
+                platform=platform.platform(),
+                app_version=APP_VERSION,
             )
             message = sensor_data.to_dict()
             send_message(message, "SensorUpdate")
@@ -83,18 +98,24 @@ def weather_sensor_loop():
         if pressure is not None and previous_pressure != pressure and ENABLE_PRESSURE:
             sensor_data = SensorUpdate(
                 source=KAFKA_PRODUCER_SOURCE_NAME + "." + "bme280",
+                probe_id=MOCK_PROBE_ID,
                 type="pressure",
                 value=pressure,
                 time_of_event=current_datetime.isoformat(),
+                device=DEVICE_NAME,
+                manufacturer=DEVICE_MANUFACTURER,
+                platform=platform.platform(),
+                app_version=APP_VERSION,
             )
             message = sensor_data.to_dict()
             send_message(message, "SensorUpdate")
             previous_pressure = pressure
 
-        time.sleep(5)
+        time.sleep(SCRAP_INTERVAL)
 
 
 def particle_sensor_loop():
+    logger.info("Starting particle sensor loop")
     pm1006 = PM1006()
 
     previous_pm25 = None
@@ -106,16 +127,21 @@ def particle_sensor_loop():
         if pm25 is not None and previous_pm25 != pm25 and ENABLE_PM25:
             sensor_data = SensorUpdate(
                 source=KAFKA_PRODUCER_SOURCE_NAME + "." + "pm1006",
+                probe_id=MOCK_PROBE_ID,
                 type="pm25",
                 value=pm25,
                 time_of_event=current_datetime.isoformat(),
+                device=DEVICE_NAME,
+                manufacturer=DEVICE_MANUFACTURER,
+                platform=platform.platform(),
+                app_version=APP_VERSION,
             )
             message = sensor_data.to_dict()
             send_message(message, "SensorUpdate")
             previous_pm25 = pm25
 
         pm1006.close_connection()
-        time.sleep(20)
+        time.sleep(SCRAP_INTERVAL)
 
 
 if __name__ == "__main__":
@@ -132,26 +158,36 @@ if __name__ == "__main__":
         rotate_interval=1,
     )
 
-    logger.info("starting weather-station")
-    logger.info("loading env file")
+    logger.info("Starting Weather Station Application v%s", APP_VERSION)
+    logger.info("Loading .env file")
     load_dotenv()
 
     try:
+        DEVICE_NAME = os.getenv("DEVICE_NAME")
+        DEVICE_MANUFACTURER = os.getenv("DEVICE_MANUFACTURER")
         KAFKA_PRODUCER_BOOTSTRAP_SERVERS = os.getenv("KAFKA_PRODUCER_BOOTSTRAP_SERVERS")
         KAFKA_PRODUCER_TOPIC = os.getenv("KAFKA_PRODUCER_TOPIC")
         KAFKA_PRODUCER_SOURCE_NAME = os.getenv("KAFKA_PRODUCER_SOURCE_NAME")
+        SCRAP_INTERVAL = int(os.getenv("SCRAP_INTERVAL"))
         ENABLE_TEMPERATURE = get_boolean_from_string(os.getenv("ENABLE_TEMPERATURE"))
         ENABLE_HUMIDITY = get_boolean_from_string(os.getenv("ENABLE_HUMIDITY"))
         ENABLE_PRESSURE = get_boolean_from_string(os.getenv("ENABLE_PRESSURE"))
         ENABLE_PM25 = get_boolean_from_string(os.getenv("ENABLE_PM25"))
     except Exception as e:
-        logger.error(f"cannot parse .env: {e}")
+        logger.error(f"Cannot parse .env file: {e}")
         sys.exit(1)
 
     config_msg = f"""configuration
+        -- metadata --
+        device_name: {DEVICE_NAME}
+        device_manufacturer: {DEVICE_MANUFACTURER}
+
+        -- kafka producer --
         topic: {KAFKA_PRODUCER_TOPIC}
         source_name: {KAFKA_PRODUCER_SOURCE_NAME}
-
+        
+        -- sensors --
+        scrap_interval: {SCRAP_INTERVAL}
         enable_temperature: {ENABLE_TEMPERATURE}
         enable_humidity: {ENABLE_HUMIDITY}
         enable_pressure: {ENABLE_PRESSURE}
@@ -161,15 +197,15 @@ if __name__ == "__main__":
     logger.info(config_msg)
 
     try:
-        logger.info("connecting to kafka")
+        logger.info("Connecting to kafka")
         producer = KafkaProducer(
             bootstrap_servers=[KAFKA_PRODUCER_BOOTSTRAP_SERVERS],
             value_serializer=lambda x: json.dumps(x).encode("utf-8"),
         )
-        logger.info("connected to kafka")
+        logger.info("Connected to kafka")
 
     except Exception as e:
-        logger.error(f"cannot connect to Kafka: {e}")
+        logger.error(f"Cannot connect to Kafka: {e}")
         sys.exit(1)
 
     try:
@@ -177,7 +213,7 @@ if __name__ == "__main__":
         thread_2 = threading.Thread(target=particle_sensor_loop)
 
         # Start the threads
-        logger.info("starting threads")
+        logger.info("Starting sensor threads")
         thread_1.start()
         thread_2.start()
         # Keep the main thread alive to handle Ctrl+C
